9c05f50720600794f7e1e5eacbade43827bd5589
rgmanikandan07 <rgmanimani0707@gmail.com>
2022-08-30 14:22:10 +0530

    add new file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var mobile={
     brand:"realme",
     model:"Narzo",
-    Ram:"4Gb",
+    Ram:"4Gb"
 };
 console.log(mobile)
 
@@ -9,7 +9,7 @@ var Tv={
     brand:"Samsung",
     model:"E213",
     prize:"30k",
-    size:"30ing",
+    size:"30ing"
 };
 console.log(Tv)
 
@@ -17,7 +17,7 @@ var labtop={
     brand:"Lenovo",
     model:"E41",
     price:"free",
-    color:"blackk",
+    color:"blackk"
 };
 console.log(labtop)
 
@@ -25,7 +25,7 @@ var bike={
     brand:"Honda",
     price:"1 laks",
     color:"redd",
-    model:"Activa",
+    model:"Activa"
 };
 console.log(bike)
 
@@ -33,7 +33,7 @@ var Car={
     brand:"Toyodo",
     price:"5laks",
     model:"Glanza",
-    color:"purple",
+    color:"purple"
 };
 console.log(Car)
 
@@ -41,7 +41,7 @@ var Headset={
     brand:"Realme",
     type:"blutooth",
     price:"400",
-    color:"Yellow",
+    color:"Yellow"
 };
 console.log(Headset)
 
@@ -49,7 +49,7 @@ var Bag={
     brand:"finencyy",
     price:"1000",
     color:"redd"
-    type:"travel",
+    type:"travel"
 };
 console.log(Bag)
 
@@ -57,7 +57,7 @@ var Note={
     brand:"orange",
     size:"one quere",
     price:"30",
-    color:"White paper",
+    color:"White paper"
 };
 console.log(Note)
 
@@ -65,7 +65,7 @@ var cycle={
     brand:"Hercules",
     type:"Big",
     price:"10k",
-    color:"black",
+    color:"black"
 };
 console.log(cycle)
 
@@ -73,7 +73,7 @@ var watch={
     brand:"fastrack",
     type:"handwatch",
     price:"300",
-    color:"White",
+    color:"White"
 };
 console,log(watch)
 
@@ -81,11 +81,171 @@ var shirt={
     brand:"Lygra",
     size:"L",
     price:"400",
-    color:"Blue",
+    color:"Blue"
 };
 console.log(shirt)
 
-var 
+var pen={
+    brand:"saino",
+    type:"Lite",
+    color:"blue",
+    price:"3"
+};
+console.log(pen)
+
+var auto={
+    brand:"bajjaj",
+    type:"3wheel",
+    price:"4laks",
+    color:"yeollow"
+};
+console.log(auto)
+
+var sketch={
+    brand:"nataraj",
+    color:"All",
+    price:"10",
+    varity:"water"
+};
+console.log(sketch)
+
+var fruit={
+    name:"apple",
+    color:"red",
+    type:"Kg",
+    price:"100"
+};
+console.log(fruit)
+
+var bus={
+    brand:"mahintra",
+    type:"shoolbus",
+    price:"30laks",
+    color:"yellow"
+};
+console.log(bus)
+
+var paper={
+    name:"A4",
+    color:"white",
+    price:"700",
+    varity:"Ruled&Unruled"
+};
+console.log(paper)
+
+var movie={
+    name:"sooraraipotru",
+    actername:"surya",
+    lang:"Tamil",
+    music:"GV"
+};
+console.log(movie)
+
+var cricket={
+    name:"cricket",
+    country:"All",
+    type:"T20,TEST,ODI",
+    start:"1575"
+};
+console.log(cricket)
+
+var dhoni={
+    name:"MS Dhoni",
+    team:"Csk",
+    color:"yellow",
+    start:"2008"
+};
+console.log(dhoni)
+
+var radio={
+    brand:"sony",
+    type:"ooffer",
+    price:"20k",
+    color:"black"
+};
+console.log(radio)
+
+var game={
+    name:"classofclan",
+    graph:"3D yellow",
+    Relise:"2012"
+    developer:"supercell",
+    mode:"multi video game"
+};
+console.log(game)
+
+var vegtable={
+    name:"vegg",
+    varity:"All",
+    vname:"tomato,potato..etc",
+    vcolor:"red,yollow..etc"
+};
+console.log(vegtable)
+
+var van={
+    brand:"Mahitra",
+    color:"lightgreen",
+    price:"10laks",
+    type:"autogeear"
+};
+console.log(van)
+
+var xylo={
+    name:"car",
+    model:"Xylo",
+    price:"20laks",
+    color:"white"
+};
+console.log(xylo)
+
+var memory={
+    name:"RAM",
+    storage:"4GB,8GB,..etc",
+    type:"Card,Inbuild,Gip",
+    price:"300"
+};
+console.log(memory)
+
+var pulb={
+    name:"LED",
+    price:"150",
+    color:"Yellow",
+    type:"2D,3D"
+};
+console.log(pulb)
+
+var tiles={
+    Brand:"MAN",
+    type:"Granaites,Elivation",
+    price:"10k",
+    color:"black"
+};
+console.log(tiles)
+
+var company={
+    Brand:"TCS",
+    founted:"1968",
+    type:"puplic",
+    industry:"IT"
+};
+console.log(company)
+
+var MyDetails={
+    Name:"Manikanda R",
+    age:"21",
+    Dist:"TENKASI",
+    ph:"123456789"
+};
+console.log(MyDetails)
+
+
+
+
+
+
+
+
+
 
 
 
